0eaf672b042acacf45fc3ebf9d38380fbce404ff
kykim818 <kykim818@naver.com>
2021-06-28 14:17:08 +0900

    Vanilla Redux start

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,10 +1,10 @@
 // import Lecture1 from "./StyledComponents/lecture1";
 // import Lecture2 from "./StyledComponents/lecture2";
 // import Lecture3 from "./StyledComponents/lecture3";
-// import Lecture4 from "./StyledComponents/lecture4";
+import Lecture4 from "./StyledComponents/lecture4";
 // import Lecture5 from "./StyledComponents/lecture5";
 // import Lecture6 from "./StyledComponents/lecture6";
-import Lecture7 from "./StyledComponents/lecture7";
+// import Lecture7 from "./StyledComponents/lecture7";
 
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
       {/* <Lecture1 /> */}
       {/* <Lecture2 /> */}
       {/* <Lecture3 /> */}
-      {/* <Lecture4 /> */}
+      <Lecture4 />
       {/* <Lecture5 /> */}
       {/* <Lecture6 /> */}
-      <Lecture7 />
+      {/* <Lecture7 /> */}
     </div>
   );
 }
